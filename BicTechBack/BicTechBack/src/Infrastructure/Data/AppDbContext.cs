564b173e33311f4ac715a9a2935622d963c650b0
emi-pereyra17 <emiacebal2012@hotmail.com>
2025-07-02 21:54:36 -0300

    Agregado de EF

---
+++ BicTechBack/BicTechBack/src/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BicTechBack.src.Core.Entities;
+
+namespace BicTechBack.src.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Carrito> Carritos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalle> PedidosDetalles { get; set; }
+        public DbSet<CarritoDetalle> CarritosDetalles { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<CategoriaMarca> CategoriasMarcas { get; set; }
+    }
+}
---
+++ BicTechBack/BicTechBack/src/Infrastructure/Repositories/ProductoRepository.cs
@@ -0,0 +1,33 @@
+﻿using BicTechBack.src.Core.Entities;
+using BicTechBack.src.Core.Interfaces;
+
+namespace BicTechBack.src.Infrastructure.Repositories
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        public Task<Producto> AddAsync(Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Producto>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Producto> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Producto> UpdateAsync(Producto producto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
