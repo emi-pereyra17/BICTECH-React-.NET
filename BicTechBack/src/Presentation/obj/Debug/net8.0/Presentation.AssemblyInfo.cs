bd7bed9d1693195d253b3aa31a19cf84051f1cfe
emi-pereyra17 <emiacebal2012@hotmail.com>
2025-09-28 14:51:59 -0300

    Implementación de Clean Arquitecture finalizada

--- BicTechBack/src/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ BicTechBack/src/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7488059881a239e15ef67ee7507d7c199b0b8e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40706856fc0e2e4beb9cae4b607d00278ebb0197")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
